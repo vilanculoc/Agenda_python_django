b66adc0b9a78bf938f62d4c67a82d7e6194e70f7
celsa Vilanculo <vilanculo.celsa@gmail.com>
2020-06-18 00:21:29 +0200

    Inclusao da funcao e tela de autenticacao

--- core/views.py
+++ core/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 #Redireccionar para agenda quando nada for informado no index. a outra parte estano url path
 # def index(request):
 #     return redirect('/agenda/')
 
+def login_user(request):
+    return render(request,'login.html')
 
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password =  request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request,'usuario ou senha invalido')
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
-    #evento = Evento.objects.filter(usuario=usuario)   # retorna os eventos do usuario logado, sessao login
+    #evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)   # retorna os eventos do usuario logado, sessao login
     response = {'eventos':evento}
     return render(request, 'agenda.html', response)
 
