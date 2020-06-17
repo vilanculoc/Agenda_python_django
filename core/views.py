287284ed027a71ae7d5a790abfb58bfddc5e4e35
celsa Vilanculo <vilanculo.celsa@gmail.com>
2020-06-17 21:44:26 +0200

    Criada a tela de listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from core.models import Evento
 # Create your views here.
+
+#Redireccionar para agenda quando nada for informado no index. a outra parte estano url path
+# def index(request):
+#     return redirect('/agenda/')
+
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    #evento = Evento.objects.filter(usuario=usuario)   # retorna os eventos do usuario logado, sessao login
+    response = {'eventos':evento}
+    return render(request, 'agenda.html', response)
+
